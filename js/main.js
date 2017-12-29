ea4703076ad064c75567264a8aef4e22d8d9c1f8
dargavavid <dargavavid@gmail.com>
2017-12-30 00:27:40 +0100

    Create div selections, add jshintrc and gitignore files

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const clockDiv = document.querySelector(".clock");
+const daysDiv = clockDiv.querySelector(".days");
+const hoursDiv = clockDiv.querySelector(".hours");
+const minutesDiv = clockDiv.querySelector(".minutes");
+const secondsDiv = clockDiv.querySelector(".seconds");
+
