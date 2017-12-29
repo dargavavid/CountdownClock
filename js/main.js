ffdd7e726ccaf8bbe8cb96b77d163020c312150f
dargavavid <dargavavid@gmail.com>
2017-12-30 00:38:31 +0100

    Add function to set countdown start date

--- js/main.js
+++ js/main.js
@@ -4,3 +4,7 @@ const hoursDiv = clockDiv.querySelector(".hours");
 const minutesDiv = clockDiv.querySelector(".minutes");
 const secondsDiv = clockDiv.querySelector(".seconds");
 
+function setCountdownStart() {
+    const now = Date.now();
+    window.localStorage.setItem("countdown-start", now);
+}
